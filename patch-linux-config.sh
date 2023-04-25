e3bded1a9f24991fd66530a756c670028d0d8290
Celeste Liu <CoelacanthusHex@gmail.com>
2023-04-25 17:46:29 +0800

    updpatch: linux 6.2.12 and enable some options
    - POWER_RESET_GPIO
    - POWER_RESET_GPIO_RESTART
    - REGULATOR_DA9063
    
    Signed-off-by: Celeste Liu <CoelacanthusHex@gmail.com>

--- patch-linux-config.sh
+++ patch-linux-config.sh
@@ -7,11 +7,11 @@ fi
 
 configs=(
     '-e' 'SOC_STARFIVE' # StarFive board support
-    '-e' 'SOC_SIFIVE' '-e' 'GPIO_SIFIVE' '-e' 'SIFIVE_CCACHE' '-e' 'EDAC_SIFIVE' '-e' 'PCIE_FU740' '-m' 'PWM_SIFIVE' # SiFive board support
+    '-e' 'SOC_SIFIVE' '-e' 'GPIO_SIFIVE' '-e' 'SIFIVE_CCACHE' '-e' 'EDAC_SIFIVE' '-e' 'PCIE_FU740' '-m' 'PWM_SIFIVE' '-m' 'REGULATOR_DA9063' # SiFive board support
     '-e' 'ERRATA_THEAD' '-e' 'ERRATA_THEAD_PBMT' # # THead board support
     '-e' 'SOC_MICROCHIP_POLARFIRE' '-e' 'PCIE_MICROCHIP_HOST' '-e' 'PCIE_XILINX' '-e' 'SERIAL_OF_PLATFORM' '-m' 'USB_MUSB_POLARFIRE_SOC' '-m' 'RTC_DRV_POLARFIRE_SOC' '-m' 'POLARFIRE_SOC_MAILBOX' '-m' 'I2C_MICROCHIP_CORE' '-m' 'POLARFIRE_SOC_SYS_CTRL' '-m' 'HW_RANDOM_POLARFIRE_SOC' # PolarFire board support
     '-e' 'SOC_VIRT' '-e' 'PCI_HOST_GENERIC' # QEMU support
-    '-m' 'MMC_DW_PLTFM' '-m' 'MMC_DW'
+    '-m' 'MMC_DW_PLTFM' '-m' 'MMC_DW' '-e' 'POWER_RESET_GPIO' '-e' 'POWER_RESET_GPIO_RESTART'
 )
 
 if [[ -z "$1" ]]; then
